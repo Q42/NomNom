228294a8b774794ae5dbfc91b6961303fa3c78d6
Jimmy Aupperlee <jimmy@q42.nl>
2020-07-12 11:45:31 +0200

    Added firestore to the mix

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,20 @@
 import * as functions from 'firebase-functions';
+import {URL, default as Url} from 'url';
+import crypto from 'crypto';
 import fetch from 'node-fetch';
 import * as domino from 'domino';
 import UserAgent from 'user-agents';
 import {getMetadata, IPageMetadata} from 'page-metadata-parser';
+
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+// Since this code will be running in the Cloud Functions environment
+// we call initialize Firestore without any arguments because it
+// detects authentication from the environment.
+const firestore = admin.firestore();
+firestore.settings({ignoreUndefinedProperties: true});
+const urlCollection = firestore.collection('url');
 
 exports.getMetadata = functions
   .region('europe-west1')
@@ -17,9 +29,17 @@ exports.getMetadata = functions
       });
       return;
     }
-    const url = req.query.url as string;
-    const result = await getData(url);
-    res.json(result);
+    const urlData = new URL(Url.parse(req.query.url as string).href);
+    const cleanedUrl = Url.resolve(urlData.origin, urlData?.pathname || '');
+    const id = crypto.createHash('sha256').update(cleanedUrl).digest('hex');
+    const existingData = await urlCollection.doc(id).get();
+    if (existingData.exists) {
+      res.json(existingData.data());
+    } else {
+      const result = await getData(cleanedUrl);
+      await urlCollection.doc(id).set(result);
+      res.json(result);
+    }
   });
 
 const getData = async (url: string) => {
